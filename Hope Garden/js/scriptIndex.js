4dd99ed5c2a34ce0e36ab74d4e1bfee153d5efbf
Paula130 <43119062+Paula130@users.noreply.github.com>
2022-08-10 18:15:42 -0400

    Delete Hope Garden directory

--- Hope Garden/js/scriptIndex.js
+++
@@ -1,8 +0,0 @@
-function barMenu(){
-    var linkBlock = document.getElementById("header-links");
-    if(linkBlock.style.display ==="block"){
-        linkBlock.style.display = "none";
-    } else {
-        linkBlock.style.display = "block";
-    }
-}
