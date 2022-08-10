1995a15b750731d43957ac786e6c8dc26a04dafe
Paula130 <43119062+Paula130@users.noreply.github.com>
2022-08-10 17:44:03 -0400

    Add files via upload

---
+++ js/scriptIndex.js
@@ -0,0 +1,8 @@
+function barMenu(){
+    var linkBlock = document.getElementById("header-links");
+    if(linkBlock.style.display ==="block"){
+        linkBlock.style.display = "none";
+    } else {
+        linkBlock.style.display = "block";
+    }
+}
